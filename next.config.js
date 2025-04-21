ce31636a4fc73b9eeba33184a3a7181fe28a8b88
Jukiho <jukiho@jukiho.com>
2025-04-21 15:34:00 +0200

    config update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // Netlify má dobrú podporu pre Next.js, takže nepotrebujeme statický export
-    // output: 'export',
-    // i18n konfigurácia nie je podporovaná v App Router, používame react-i18next namiesto toho
-    // i18n: {
-    //     locales: ['en', 'de', 'sk'],
-    //     defaultLocale: 'en'
-    // }
+    generateBuildId: async () => {
+        return 'build-' + new Date().getTime();
+    },
+    outputFileTracingExcludes: {
+        '*': ['node_modules/**/*']
+    }
 };
 
 module.exports = nextConfig;
